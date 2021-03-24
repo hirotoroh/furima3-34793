7f08f0a08136e44487ace3f2da376d7320e01f4d
hirotoroh <i.hiroto.0815@icloud.com>
2021-03-24 15:44:14 +0900

    スペース削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :encrypted_password, :password, format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
+  validates :encrypted_password, :password, format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
         
   with_options presence: true do
     validates :nickname
