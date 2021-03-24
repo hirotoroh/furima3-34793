b64e06ae7c14b685c5f0ac1be1f4c2547cae0df9
hirotoroh <i.hiroto.0815@icloud.com>
2021-03-25 08:58:16 +0900

    コントローラー記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,5 +7,17 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :status_id, :shipping_area_id, :shipping_cost_id, :shipping_day_id, :price)
   end
 end
