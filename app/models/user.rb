580325bcfecdfebdd9d42a1884c3dcbfa3793256
hirotoroh <i.hiroto.0815@icloud.com>
2021-03-24 17:17:29 +0900

    バリデーション変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,15 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+
+    with_options format: { with: /\A[ぁ-んァ-ン一-龠々]+\z/ } do
+      validates :family_name
+      validates :last_name
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+      validates :family_name_kana
+      validates :last_name_kana
+    end
     validates :birth_day
   end
 end
