13f2af7d85565769d91f8ae2f14a26c407b94466
hirotoroh <i.hiroto.0815@icloud.com>
2021-03-25 14:03:53 +0900

    priceバリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,7 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   validates :name, :description, presence: true
+  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}, format: { with: /\A[0-9]+\z/ }
 
   validates :category_id, numericality: { other_than: 0 } 
   validates :status_id, numericality: { other_than: 0 } 
