2d58b913ce8a666b2717977aa6eb76dcecb837e3
hirotoroh <i.hiroto.0815@icloud.com>
2021-03-25 09:08:12 +0900

    idづけ

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,9 +9,9 @@ class Item < ApplicationRecord
 
   varidates :name, :description, presence: true
 
-  validates :category, numericality: { other_than: 1 } 
-  validates :status, numericality: { other_than: 1 } 
-  validates :shipping_cost, numericality: { other_than: 1 } 
-  validates :shipping_area, numericality: { other_than: 1 } 
-  validates :shipping_day, numericality: { other_than: 1 } 
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :status_id, numericality: { other_than: 1 } 
+  validates :shipping_cost_id, numericality: { other_than: 1 } 
+  validates :shipping_area_id, numericality: { other_than: 1 } 
+  validates :shipping_day_id, numericality: { other_than: 1 } 
 end
