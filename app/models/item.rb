0f20d57d4bb4407dc2e294db7e4453caa5757117
hirotoroh <i.hiroto.0815@icloud.com>
2021-03-25 15:24:37 +0900

    with_options設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,9 +10,11 @@ class Item < ApplicationRecord
   validates :name, :description, :image, presence: true
   validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}, format: { with: /\A[0-9]+\z/ }
 
-  validates :category_id, numericality: { other_than: 0 } 
-  validates :status_id, numericality: { other_than: 0 } 
-  validates :shipping_cost_id, numericality: { other_than: 0 } 
-  validates :shipping_area_id, numericality: { other_than: 0 } 
-  validates :shipping_day_id, numericality: { other_than: 0 } 
+  with_options numericality: { other_than: 0 } do
+    validates :category_id 
+    validates :status_id
+    validates :shipping_cost_id 
+    validates :shipping_area_id 
+    validates :shipping_day_id 
+  end
 end
