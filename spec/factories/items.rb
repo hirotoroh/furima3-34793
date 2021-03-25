7e3da1f9518a32f741e81adffa06d23009c7ce24
hirotoroh <i.hiroto.0815@icloud.com>
2021-03-25 15:01:07 +0900

    itemテストコード記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
-    
+    name {"車です"}
+    description {"乗りやすいです"}
+    category_id {1}
+    status_id {1}
+    shipping_cost_id {1}
+    shipping_area_id {1}
+    shipping_day_id {1}
+    price {300}
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,75 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+  describe '商品出品機能' do
+    context '出品できるとき' do
+      it '全てが存在すれば出品できること' do
+        expect(@item).to be_valid
+      end
+    end
+    context '商品出品できないとき' do
+      it '商品名が空では登録できないこと' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+
+      it '商品の説明が空では登録できないこと' do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+
+      it 'カテゴリーが空では登録できないこと' do
+        @item.category_id = '0'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 0")
+      end
+
+      it '商品の状態が空では登録できないこと' do
+        @item.status_id = '0'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status must be other than 0")
+      end
+
+      it '配送料の負担が空では登録できないこと' do
+        @item.shipping_cost_id = '0'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping cost must be other than 0")
+      end
+
+      it '発送元の地域が空では登録できないこと' do
+        @item.shipping_area_id = '0'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping area must be other than 0")
+      end
+
+      it '発送までの日数が空では登録できないこと' do
+        @item.shipping_day_id = '0'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping day must be other than 0")
+      end
+
+      it '価格が空では登録できないこと' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+
+      it '価格が300円以下では登録できないこと' do
+        @item.price = '299'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+
+      it '価格が9,999,999円以上では登録できないこと' do
+        @item.price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+    end
+  end
 end
