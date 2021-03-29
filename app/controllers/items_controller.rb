a3eb48ec5c9287c06d1fe34d59d456e9a0d32b4d
hirotoroh <i.hiroto.0815@icloud.com>
2021-03-29 14:59:40 +0900

    リファクタリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :authenticate_user!, except: [:show, :index]
 
   def index
     @items = Item.order(id: :DESC).includes(:user)
