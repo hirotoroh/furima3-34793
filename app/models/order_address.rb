aada7f6c23002b849d73a3b0bfbe6f8b14f75e3d
hirotoroh <i.hiroto.0815@icloud.com>
2021-03-30 15:55:19 +0900

    処理記述

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -14,5 +14,8 @@ class OrderAddress
   }
   end
 
-  
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, shipping_area_id: shipping_area_id, city: city, house_number: house_number, build_name: build_name, phone_number: phone_number, order_id: order.id)
+  end
 end
