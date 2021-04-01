1076c3c6600cb10816bc55165cf5848a8c988f27
hirotoroh <i.hiroto.0815@icloud.com>
2021-04-01 15:44:05 +0900

    indexアクション定義

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,12 @@
 class OrdersController < ApplicationController
 
   def index
+    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
   def create
-    
+    binding.pry
   end
 
 end
