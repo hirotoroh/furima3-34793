70f1c33264148192f730303d2d2d1288d1acfbed
hirotoroh <i.hiroto.0815@icloud.com>
2021-04-01 16:02:26 +0900

    createアクション記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,6 +6,19 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :shipping_area_id, :city, :house_number, :build_name, :phone_number, :order).merge(user_id: current_user.id)
   end
 
 end
