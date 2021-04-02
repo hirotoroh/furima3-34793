ad53acf431d5703c07051f95171f71a0d287870f
hirotoroh <i.hiroto.0815@icloud.com>
2021-04-02 11:31:29 +0900

    テストコード記述

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe OrderAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.create(:item)
+    @order_address = FactoryBot.build(:order_address, user_id: @user.id, item_id: @item.id)
+    # sleep 1
+  end
+  describe '商品購入機能' do
+    context '購入できるとき' do
+      it '全てが存在すれば購入できること' do
+        expect(@order_address).to be_valid
+      end
+    end
+  end
 end
