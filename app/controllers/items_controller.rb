cbe34fe742c25caca60daf9713db8b4dd4dcd964
hirotoroh <i.hiroto.0815@icloud.com>
2021-04-02 19:43:51 +0900

    制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,6 +26,9 @@ class ItemsController < ApplicationController
     unless @item.user_id == current_user.id
       redirect_to action: :index
     end
+    unless @item.order == nil
+      redirect_to root_path
+    end
   end
 
   def update
