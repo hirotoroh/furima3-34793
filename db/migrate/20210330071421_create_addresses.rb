40448dfead252ca93b7e74ba8630c6f3567f0e49
hirotoroh <i.hiroto.0815@icloud.com>
2021-04-04 14:46:34 +0900

    マイグレート削除

--- db/migrate/20210330071421_create_addresses.rb
+++ db/migrate/20210330071421_create_addresses.rb
@@ -5,7 +5,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
       t.integer    :shipping_area_id, null: false
       t.string     :city,             null: false
       t.string     :house_number,     null: false
-      t.string     :build_name,       null: false
+      t.string     :build_name
       t.string     :phone_number,     null: false
       t.references :order,            foreign_key: true
       t.timestamps
